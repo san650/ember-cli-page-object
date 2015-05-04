23fb13f115d810bfdee8d25b6f646ae21d3cd18c
Santiago Ferreira <san650@gmail.com>
2015-05-03 21:16:12 -0300

    Add tests for not-has-class

---
+++ tests/unit/predicates/has-class-test.js
@@ -0,0 +1,96 @@
+import { module, test as it } from 'qunit';
+import { notHasClass } from 'page-object/predicates';
+
+module('Predicates - notHasClass', {
+  beforeEach: function() {
+    window.findWithAssert = function(selector) {
+      var element = $('#ember-testing').find(selector);
+
+      if (element.length === 0) {
+        throw 'Element not found';
+      }
+
+      return element;
+    };
+  },
+
+  afterEach: function() {
+    delete window.findWithAssert;
+    $('#ember-testing').html('');
+  }
+});
+
+it('responds to build', function(assert) {
+  var builder = notHasClass();
+
+  assert.ok($.isFunction(builder.build), '`build` is a function');
+});
+
+it('returns a builder function', function(assert) {
+  var builder = notHasClass(),
+      predicate = builder.build('dummy', {});
+
+  assert.ok($.isFunction(predicate), '`build()` is a function');
+});
+
+it('returns false when the element has the class', function(assert) {
+  $('<div>', {
+    'class': 'element has-error'
+  }).appendTo('#ember-testing');
+
+  var predicate = notHasClass('has-error', '.element').build('key', {});
+
+  assert.ok(!predicate());
+});
+
+it('returns true when the element doesn\'t have the class', function(assert) {
+  $('<div>', {
+    'class': 'element'
+  }).appendTo('#ember-testing');
+
+  var predicate = notHasClass('has-error', '.element').build('key', {});
+
+  assert.ok(predicate());
+});
+
+it('raises an error when the element doesn\'t exist', function(assert) {
+  assert.expect(1);
+
+  try {
+    let predicate = notHasClass('has-error', '.element').build('key', {});
+
+    predicate();
+  } catch(e) {
+    assert.ok(true, 'Element not found');
+  }
+});
+
+it('uses scope', function(assert) {
+  $('<div>', {
+    'class': 'element scope has-error'
+  })
+    .appendTo('#ember-testing')
+    .append(
+      $('<div>', {
+        'class': 'element'
+      }));
+
+  var predicate = notHasClass('has-error', '.element:first', { scope: '.scope' }).build('key', {});
+
+  assert.ok(predicate());
+});
+
+it('uses page scope', function(assert) {
+  $('<div>', {
+    'class': 'element scope has-error'
+  })
+    .appendTo('#ember-testing')
+    .append(
+      $('<div>', {
+        'class': 'element'
+      }));
+
+  var predicate = notHasClass('has-error', '.element:first').build('key', { scope: '.scope' });
+
+  assert.ok(predicate());
+});
