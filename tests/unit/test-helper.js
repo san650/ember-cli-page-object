e8009f984f44182d02968c2b8936b13def70fbf4
Santiago Ferreira <san650@gmail.com>
2015-05-05 21:38:55 -0300

    Remove unneeded findWithAssert stub

--- tests/unit/test-helper.js
+++ tests/unit/test-helper.js
@@ -1,26 +1,8 @@
 import { module, test } from 'qunit';
 
-function findWithAssert(selector) {
-  var element = $('#ember-testing').find(selector);
-
-  if (element.length === 0) {
-    throw 'Element not found';
-  }
-
-  return element;
-}
-
 export function moduleFor(category, helperName) {
   module(`${category} - .${helperName}`, {
-    beforeEach: function() {
-      // Mock findWithAssert
-      window.findWithAssert = findWithAssert;
-    },
-
     afterEach: function() {
-      // Delete mock
-      delete window.findWithAssert;
-
       // Cleanup DOM
       $('#ember-testing').html('');
     }
