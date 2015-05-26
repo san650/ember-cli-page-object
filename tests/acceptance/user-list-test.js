5bbd04604d77a04d921f9336c7dc26510a15b342
Juan Azambuja <juan@wyeworks.com>
2015-05-26 10:10:00 -0300

    Add selectable helper

--- tests/acceptance/user-list-test.js
+++ tests/acceptance/user-list-test.js
@@ -23,20 +23,25 @@ var page = PO.build({
     itemScope: 'tbody tr',
     item: {
       userName: PO.text('td', { index: 1 }),
-      role: PO.text('td', { index: 2})
+      role: PO.text('td', { index: 2}),
+      selectGender: PO.selectable('select'),
+      gender: PO.text('select option:selected')
     }
   })
 });
 
 test('Page contents', function(assert) {
-  assert.expect(4);
+  assert.expect(5);
 
   page.visit();
+
+  page.users(1).selectGender('Female');
 
   andThen(function() {
     assert.equal(page.title(), 'Users');
     assert.equal(page.users().count(), 2);
     assert.equal(page.users(1).userName(), 'jane');
     assert.equal(page.users(1).role(), 'admin');
+    assert.equal(page.users(1).gender(), 'Female');
   });
 });
