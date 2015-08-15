41f54f091e2a85020bcd24183eb7f1acf9ab891c
Santiago Ferreira <san650@gmail.com>
2015-08-15 19:05:48 -0300

    Fix template variable name
    Also avoid Redefinition of '$'

--- blueprints/page-object-helper/files/tests/pages/helpers/__name__.js
+++ blueprints/page-object-helper/files/tests/pages/helpers/__name__.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 import PageObject from '../../page-object';
 
-let $ = Ember.$;
-
-function <%= camelizeModuleName %>(selector /*, options */) {
-  return $(selector).prop('checked');
+function <%= camelizedModuleName %>(selector /*, options */) {
+  return Ember.$(selector).prop('checked');
 }
 
-export default PageObject.customHelper(<%= camelizeModuleName %>);
+export default PageObject.customHelper(<%= camelizedModuleName %>);
