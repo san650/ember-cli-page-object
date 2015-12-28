087ac43a468b0833017c0695a8b3cdb0e5b2c2cd
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-28 01:20:27 -0600

    Allow nested pages

--- blueprints/page-object/index.js
+++ blueprints/page-object/index.js
@@ -1,3 +1,12 @@
+var stringUtils = require('ember-cli-string-utils');
+
 module.exports = {
-  description: 'Generates a page object for acceptance tests.'
+  description: 'Generates a page object for acceptance tests.',
+  locals: function(options) {
+    var testFolderRoot = stringUtils.dasherize(options.project.name());
+
+    return {
+      testFolderRoot: testFolderRoot,
+    };
+  }
 };
