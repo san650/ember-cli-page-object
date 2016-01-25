bd1b2caff9c1aae5fe3d6453c4e9ca3a0d59c054
Santiago Ferreira <san650@gmail.com>
2016-01-24 22:27:34 -0300

    Add default properties to components

--- test-support/page-object/create.js
+++ test-support/page-object/create.js
@@ -1,5 +1,75 @@
+import Ember from 'ember';
 import Ceibo from 'ceibo';
+import { text } from './properties/text';
+import { isVisible } from './properties/is-visible';
+import { isHidden } from './properties/is-hidden';
+import { clickOnText } from './properties/click-on-text';
+import { clickable } from './properties/clickable';
+import { contains } from './properties/contains';
 
-export function create(definition, options = null) {
-  return Ceibo.create(definition, options);
+var { merge } = Ember;
+
+function plugDefaultProperties(definition) {
+  if (typeof(definition.isVisible) === 'undefined') {
+    definition.isVisible = isVisible();
+  }
+
+  if (typeof(definition.isHidden) === 'undefined') {
+    definition.isHidden = isHidden();
+  }
+
+  if (typeof(definition.clickOn) === 'undefined') {
+    definition.clickOn = clickOnText();
+  }
+
+  if (typeof(definition.click) === 'undefined') {
+    definition.click = clickable();
+  }
+
+  if (typeof(definition.contains) === 'undefined') {
+    definition.contains = contains();
+  }
+
+  if (typeof(definition.text) === 'undefined') {
+    definition.text = text();
+  }
+}
+
+/**
+ * See https://github.com/san650/ceibo#examples for more info on how Ceibo
+ * builders work.
+ */
+function buildObject(builder, target, key, definition) {
+  var container = {};
+
+  plugDefaultProperties(definition);
+
+  // Create child component
+  Ceibo.defineProperty(target, key, container);
+
+  // Recursion
+  builder.processNode(definition, container, target);
+}
+
+/**
+ * Creates a new PageObject
+ *
+ * @example
+ *
+ *   var page = PageObject.create({
+ *     title: text('.title')
+ *   });
+ *
+ *   assert.equal(page.title, 'Dummy title');
+ *
+ * @param {Object} definition - PageObject definition
+ * @param {Object} options - [private] Ceibo options. Do not use!
+ * @return {PageObject}
+ */
+export function create(definition, options = {}) {
+  var builder = {
+    object: buildObject
+  };
+
+  return Ceibo.create(definition, merge({ builder }, options ));
 }
--- tests/unit/create-test.js
+++ tests/unit/create-test.js
@@ -22,6 +22,7 @@ test('resets scope', function(assert) {
       <span class="scope">Lorem</span>
     </div>
   `);
+
   var page = create({
     scope: '.invalid-scope',
 
@@ -33,4 +34,89 @@ test('resets scope', function(assert) {
   });
 
   assert.equal(page.foo.bar, 'Lorem');
+});
+
+test('generates .isVisible property', function(assert) {
+  fixture('Lorem <span>ipsum</span>');
+
+  var page = create({
+    scope: 'span',
+    foo: {
+    }
+  });
+
+  assert.ok(page.isVisible, 'page is visible');
+  assert.ok(page.foo.isVisible, 'component is visible');
+});
+
+test('generates .isHidden property', function(assert) {
+  fixture('Lorem <span style="display:none">ipsum</span>');
+
+  var page = create({
+    scope: 'span',
+    foo: {
+    }
+  });
+
+  assert.ok(page.isHidden, 'page is hidden');
+  assert.ok(page.foo.isHidden, 'component is hidden');
+});
+
+test('generates .clickOn property', function(assert) {
+  assert.expect(1);
+
+  window.click = function() {
+    assert.ok(true, 'click called');
+  };
+
+  var page = create({
+    foo: {
+    }
+  });
+
+  page.foo.clickOn();
+});
+
+test('generates .click property', function(assert) {
+  assert.expect(1);
+
+  window.click = function() {
+    assert.ok(true, 'click called');
+  };
+
+  var page = create({
+    foo: {
+    }
+  });
+
+  page.foo.click();
+});
+
+test('generates .click property', function(assert) {
+  fixture('Ipsum <span>Dolor</span>');
+
+  var page = create({
+    foo: {
+      scope: 'span'
+    }
+  });
+
+  assert.ok(page.foo.contains('or'), 'contains');
+});
+
+test('generates .text property', function(assert) {
+  fixture(`
+    <div>Lorem</div>
+    <div class="scope">Ipsum <span>Dolor</span></div>
+  `);
+
+  var page = create({
+    scope: '.scope',
+    foo: {
+      scope: 'span'
+    }
+  });
+
+  assert.equal(page.text, 'Ipsum Dolor');
+  assert.equal(page.foo.text, 'Dolor');
 });
