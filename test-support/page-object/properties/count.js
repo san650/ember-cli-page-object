33145171354123c9aba36a63e1064b2a30b83eca
Juan Azambuja <juan@wyeworks.com>
2016-02-10 20:14:40 -0300

    Use jsdoc documentation on count property

--- test-support/page-object/properties/count.js
+++ test-support/page-object/properties/count.js
@@ -8,17 +8,70 @@ var $ = Ember.$;
  *
  * @example
  *
- *   var page = PageObject.create({
- *     imageCount: count('.img')
- *   });
+ * // <span>1</span>
+ * // <span>2</span>
  *
- *   assert.equal(page.imageCount(), 2);
+ * var page = PageObject.create({
+ *   spanCount: PageObject.count('span')
+ * });
  *
- * @param {string} selector - CSS selector of the element to check
+ * // returns 2
+ * page.spanCount
+ *
+ * @example
+ *
+ * // <div>Text</div>
+ *
+ * var page = PageObject.create({
+ *   spanCount: PageObject.count('span')
+ * })
+ *
+ * // returns 0
+ * page.spanCount
+ *
+ * @example
+ *
+ * // <div><span></span></div>
+ * // <div class="scope"><span></span><span></span></div>
+ *
+ * var page = PageObject.create({
+ *   spanCount: PageObject.count('span', { scope: '.scope' })
+ * })
+ *
+ * // returns 2
+ * page.spanCount
+ *
+ * @example
+ *
+ * // <div><span></span></div>
+ * // <div class="scope"><span></span><span></span></div>
+ *
+ * var page = PageObject.create({
+ *   scope: '.scope',
+ *   spanCount: PageObject.count('span')
+ * })
+ *
+ * // returns 2
+ * page.spanCount
+ *
+ * @example
+ *
+ * // <div><span></span></div>
+ * // <div class="scope"><span></span><span></span></div>
+ *
+ * var page = PageObject.create({
+ *   scope: '.scope',
+ *   spanCount: PageObject.count('span', { resetScope: true })
+ * })
+ *
+ * // returns 1
+ * page.spanCount
+ *
+ * @param {string} selector - CSS selector of the element or elements to check
  * @param {Object} options - Additional options
  * @param {string} options.scope - Add scope
  * @param {boolean} options.resetScope - Ignore parent scope
- * @return {Descriptor}
+ * @return {Descriptor} - Descriptor that returns amount of elements matched by selector
  */
 export function count(selector, options = {}) {
   return {
