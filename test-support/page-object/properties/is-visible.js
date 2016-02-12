5a0eb834d1c39d2dcd07b6610e9292a05c318a85
Juan Azambuja <juan@wyeworks.com>
2016-02-12 19:18:04 -0300

    Use jsdoc documentation on is-visible property

--- test-support/page-object/properties/is-visible.js
+++ test-support/page-object/properties/is-visible.js
@@ -1,21 +1,88 @@
 import { findElement, every } from '../helpers';
 
 /**
- * Creates a predicate to validate if an element is visible
+ * Validates if an element or set of elements are visible
  *
  * @example
  *
- *   var page = PageObject.create({
- *     isImageVisible: isVisible('.img')
- *   });
+ * // Lorem <span>ipsum</span>
  *
- *   assert.ok(page.isImageVisible(), 'Image is visible');
+ * let page = PageObject.create({
+ *   spanIsVisible: PageObject.isVisible('span')
+ * });
+ *
+ * assert.ok(page.spanIsVisible);
+ *
+ * @example
+ *
+ * // <span>ipsum</span>
+ * // <span style="display:none">dolor</span>
+ *
+ * let page = PageObject.create({
+ *   spansAreVisible: PageObject.isVisible('span', { multiple: true })
+ * });
+ *
+ * // not all spans are visible
+ * assert.ok(!page.spansAreVisible);
+ *
+ * @example
+ *
+ * // <span>ipsum</span>
+ * // <span>dolor</span>
+ *
+ * let page = PageObject.create({
+ *   spansAreVisible: PageObject.isVisible('span', { multiple: true })
+ * });
+ *
+ * // all spans are visible
+ * assert.ok(page.spansAreVisible);
+ *
+ * @example
+ *
+ * // Lorem <div>ipsum</div>
+ *
+ * let page = PageObject.create({
+ *   spanIsVisible: PageObject.isHidden('span')
+ * });
+ *
+ * // returns false when element doesn't exist in DOM
+ * assert.ok(!page.spanIsVisible);
+ *
+ * @example
+ *
+ * // <div><span style="display:none">lorem</span></div>
+ *
+ * <div class="scope"><span>ipsum</span></div>
+ *
+ * let page = PageObject.create({
+ *   spanIsVisible: PageObject.isHidden('span', { scope: '.scope' })
+ * });
+ *
+ * assert.ok(page.spanIsVisible);
+ *
+ * @example
+ *
+ * // <div><span style="display:none">lorem</span></div>
+ * // <div class="scope"><span>ipsum</span></div>
+ *
+ * let page = PageObject.create({
+ *   scope: '.scope',
+ *   spanIsVisible: PageObject.isHidden('span')
+ * });
+ *
+ * assert.ok(page.spanIsVisible);
+ *
+ * @public
  *
  * @param {string} selector - CSS selector of the element to check
  * @param {Object} options - Additional options
- * @param {string} options.scope - Overrides parent scope
+ * @param {string} options.scope - Nests provided scope with parent's scope
  * @param {number} options.at - Reduce the set of matched elements to the one at the specified index
+ * @param {boolean} options.resetScope - Override parent's scope
+ * @param {boolean} options.multiple - Check if all elements matched by selector are visible
  * @return {Descriptor}
+ *
+ * @throws Will throw an error if multiple elements are matched by selector and multiple options is not set
  */
 export function isVisible(selector, options = {}) {
   return {
