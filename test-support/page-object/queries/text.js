734d5a4867f4bb14b75d22a253598fb573fd9987
Jerad Gallinger <jerad@jeradgallinger.ca>
2016-03-07 15:34:00 -0500

    Re-add JSDoc documentation that vanished during merge

--- test-support/page-object/queries/text.js
+++ test-support/page-object/queries/text.js
@@ -1,29 +1,68 @@
 import { findElementWithAssert, map, normalizeText } from '../helpers';
 
 /**
- * Gets the text of the matched element
+ * Returns text of the element or Array of texts of all matched elements by selector.
  *
  * @example
  *
- *   var page = PageObject.create({
- *     title: text('h1')
- *   });
+ * // Hello <span>world!</span>
  *
- *   assert.equal(page.title, 'Page title');
+ * const page = PageObject.create({
+ *   text: PageObject.text('span')
+ * });
  *
- *   var page = PageObject.create({
- *     options: text('li', { multiple: true })
- *   });
+ * assert.equal(page.text, 'world!');
  *
- *   assert.deepEqual(page.options, ['lorem', 'ipsum'])
+ * @example
+ *
+ * // <span>lorem</span>
+ * // <span> ipsum </span>
+ * // <span>dolor</span>
+ *
+ * const page = PageObject.create({
+ *   texts: PageObject.text('span', { multiple: true })
+ * });
+ *
+ * assert.deepEqual(page.texts, ['lorem', 'ipsum', 'dolor']);
+ *
+ * @example
+ *
+ * // <div><span>lorem</span></div>
+ * // <div class="scope"><span>ipsum</span></div>
+ * // <div><span>dolor</span></div>
+ *
+ * const page = PageObject.create({
+ *   text: PageObject.text('span', { scope: '.scope' })
+ * });
+ *
+ * assert.equal(page.text, 'ipsum');
+ *
+ * @example
+ *
+ * // <div><span>lorem</span></div>
+ * // <div class="scope"><span>ipsum</span></div>
+ * // <div><span>dolor</span></div>
+ *
+ * const page = PageObject.create({
+ *   scope: '.scope',
+ *   text: PageObject.text('span')
+ * });
+ *
+ * // returns 'ipsum'
+ * assert.equal(page.text, 'ipsum');
+ *
+ * @public
  *
  * @param {string} selector - CSS selector of the element to check
  * @param {Object} options - Additional options
- * @param {string} options.scope - Overrides parent scope
+ * @param {string} options.scope - Nests provided scope within parent's scope
  * @param {number} options.at - Reduce the set of matched elements to the one at the specified index
- * @param {boolean} options.resetScope - Ignore parent scope
+ * @param {boolean} options.resetScope - Override parent's scope
  * @param {boolean} options.multiple - Return an array of values
  * @return {Descriptor}
+ *
+ * @throws Will throw an error if no element matches selector
+ * @throws Will throw an error if multiple elements are matched by selector and multiple option is not set
  */
 export function text(selector, options = {}) {
   return {
--- test-support/page-object/queries/value.js
+++ test-support/page-object/queries/value.js
@@ -1,22 +1,64 @@
 import { findElementWithAssert, map } from '../helpers';
 
 /**
- * Gets the value of the matched element
+ * Returns the value of a matched element, or an array of values of all matched elements.
  *
  * @example
  *
- *   var page = PageObject.create({
- *     search: value('input')
- *   });
+ * // <input value="Lorem ipsum">
  *
- *   assert.equal(page.search, 'search term');
+ * const page = PageObject.create({
+ *   value: PageObject.value('input')
+ * });
+ *
+ * assert.equal(page.value, 'Lorem ipsum');
+ *
+ * @example
+ *
+ * // <input value="lorem">
+ * // <input value="ipsum">
+ *
+ * const page = PageObject.create({
+ *   value: PageObject.value('input', { multiple: true })
+ * });
+ *
+ * assert.deepEqual(page.value, ['lorem', 'ipsum']);
+ *
+ * @example
+ *
+ * // <div><input value="lorem"></div>
+ * // <div class="scope"><input value="ipsum"></div>
+ *
+ * const page = PageObject.create({
+ *   value: PageObject.value('input', { scope: '.scope' })
+ * });
+ *
+ * assert.equal(page.value, 'ipsum');
+ *
+ * @example
+ *
+ * // <div><input value="lorem"></div>
+ * // <div class="scope"><input value="ipsum"></div>
+ *
+ * const page = PageObject.create({
+ *   scope: '.scope',
+ *   value: PageObject.value('input')
+ * });
+ *
+ * assert.equal(page.value, 'ipsum');
+ *
+ * @public
  *
  * @param {string} selector - CSS selector of the element to check
  * @param {Object} options - Additional options
- * @param {string} options.scope - Overrides parent scope
+ * @param {string} options.scope - Nests provided scope within parent's scope
+ * @param {boolean} options.resetScope - Override parent's scope
  * @param {number} options.at - Reduce the set of matched elements to the one at the specified index
- * @param {boolean} options.resetScope - Ignore parent scope
+ * @param {boolean} options.multiple - If set, the function will return an array of values
  * @return {Descriptor}
+ *
+ * @throws Will throw an error if no element matches selector
+ * @throws Will throw an error if multiple elements are matched by selector and multiple option is not set
  */
 export function value(selector, options = {}) {
   return {
