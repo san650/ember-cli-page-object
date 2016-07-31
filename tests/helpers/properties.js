2a2aaf490115155c5781a5d7a37d18df3d9d0e59
Santiago Ferreira <san650@gmail.com>
2016-07-31 13:24:59 -0300

    Migrate 'visitable' test

--- tests/helpers/properties.js
+++ tests/helpers/properties.js
@@ -1,7 +1,10 @@
 import { AcceptanceAdapter, moduleForAcceptance, testForAcceptance } from './properties/acceptance-adapter';
 import { IntegrationAdapter, moduleForIntegration, testForIntegration } from './properties/integration-adapter';
 
-export function moduleForProperty(name, cb) {
+export function moduleForProperty(name, cbOrOptions, cb) {
+  let options = cb ? cbOrOptions : {};
+  cb = cb || cbOrOptions;
+
   // Generate acceptance tests
   let acceptanceAdapter = new AcceptanceAdapter();
   moduleForAcceptance(`Acceptance mode | Property | ${name}`, {
@@ -10,6 +13,10 @@ export function moduleForProperty(name, cb) {
     }
   });
   cb(testForAcceptance, acceptanceAdapter);
+
+  if (options.acceptanceOnly) {
+    return;
+  }
 
   // Generate integration tests
   let integrationAdapter = new IntegrationAdapter();
