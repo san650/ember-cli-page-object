40f56dd43ee393a71245640d77cbe50e5d0e6368
Santiago Ferreira <san650@gmail.com>
2016-08-16 22:10:21 -0300

    Update blueprint tests

--- node-tests/blueprints/page-object-component-test.js
+++ node-tests/blueprints/page-object-component-test.js
@@ -1,49 +1,34 @@
 'use strict';
 
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
-var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
-var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+var blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks = blueprintHelpers.setupTestHooks;
+var emberNew = blueprintHelpers.emberNew;
+var emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+var expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
 describe('Acceptance: ember generate and destroy page-object', function() {
   setupTestHooks(this);
 
-  it('generates a page-object component blueprint in an ember app', function() {
-    // pass any additional command line options in the arguments array
-    return generateAndDestroy(['page-object-component', 'foo'], {
-      // define files to assert, and their contents
-      target: 'app',
-      files: [{
-        file: 'tests/pages/components/foo.js',
-        contains: [
-          'import {',
-          '  text',
-          "} from 'ember-cli-page-object';",
-          '',
-          'export default {',
-          "  title: text('h1')",
-          '};'
-        ]
-      }]
-    });
+  it('generates a page-object component in an ember app', function() {
+    var args = ['page-object-component', 'foo'];
+
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, (file) => {
+        expect(file('tests/pages/components/foo.js'))
+          .to.contain("from 'ember-cli-page-object';")
+          .to.contain('export default {');
+    }));
   });
 
-  it('generates a page-object component blueprint in an ember addon', function() {
-    // pass any additional command line options in the arguments array
-    return generateAndDestroy(['page-object-component', 'bar'], {
-      // define files to assert, and their contents
-      target: 'addon',
-      files: [{
-        file: 'tests/pages/components/bar.js',
-        contains: [
-          'import {',
-          '  text',
-          "} from 'ember-cli-page-object';",
-          '',
-          'export default {',
-          "  title: text('h1')",
-          '};'
-        ]
-      }]
-    });
+  it('generates a page-object component in an ember addon', function() {
+    var args = ['page-object-component', 'foo'];
+
+    return emberNew({ target: 'addon' })
+      .then(() => emberGenerateDestroy(args, (file) => {
+        expect(file('tests/pages/components/foo.js'))
+          .to.contain("from 'ember-cli-page-object';")
+          .to.contain('export default {');
+    }));
   });
 });
