e413345e0aa5728033625e7019144c52e6856476
Nikita Gusarov <nickgusarov@gmail.com>
2017-08-05 15:11:44 +0300

    Add ability for mupltiple extending

--- addon/-private/-extend.js
+++ addon/-private/-extend.js
@@ -1,16 +1,45 @@
+import Ember from 'ember';
 import { create } from './create';
 import { assign } from './helpers';
 
-export function extend(...definitions) {
-  const mergedDefinitions = assign({}, ...definitions);
+const {
+  copy
+} = Ember;
+
+export function extend(originalDefinition) {
+  const _extend = extend;
 
   return {
+    create(definitionOrUrl, definitionOrOptions, optionsOrNothing) {
+      let definition;
+      let url;
+      let options;
 
-    // TODO: Change API to create-compatible
-    create(properties) {
-      const mergedProperties = assign({}, mergedDefinitions, properties);
-      return create(mergedProperties);
+      if (typeof (definitionOrUrl) === 'string') {
+        url = definitionOrUrl;
+        definition = definitionOrOptions || {};
+        options = optionsOrNothing || {};
+      } else {
+        url = false;
+        definition = definitionOrUrl;
+        options = definitionOrOptions || {};
+      }
+
+      const parentDefinition = copy(originalDefinition);
+      const mergedDefinition = assign(parentDefinition, definition);
+
+      return create(
+        url || mergedDefinition,
+        mergedDefinition || options,
+        options
+      );
     },
 
+    extend(definition) {
+      const parentDefinition = copy(originalDefinition);
+      const mergedDefinition = assign(parentDefinition, definition);
+
+      return _extend(mergedDefinition);
+    }
   };
 }
