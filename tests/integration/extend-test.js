a25ecb690dac62c8809a01fcf08b0d5f874b5f85
Nikita Gusarov <nickgusarov@gmail.com>
2017-08-05 15:11:57 +0300

    Add test for multiple extending

--- tests/integration/extend-test.js
+++ tests/integration/extend-test.js
@@ -7,7 +7,7 @@ moduleForComponent('calculating-device', 'Integration | extend', {
   integration: true
 });
 
-test('Supports nested objects', function(assert) {
+test('Supports extending', function(assert) {
   const ScreenPageObject = PageObject.extend({
     isNine() {
       return this.text === '9';
@@ -19,6 +19,40 @@ test('Supports nested objects', function(assert) {
 
     getScreen() {
       return ScreenPageObject.create({
+        context: this.context,
+        scope: '.screen',
+      });
+    },
+
+  });
+
+  const isNine = page
+    .render(createCalculatorTemplate())
+    .clickOn('9')
+    .getScreen()
+    .isNine();
+
+  assert.ok(isNine, 'should be nine');
+});
+
+test('Supports multiple extending', function(assert) {
+  const ScreenPageObject = PageObject.extend({
+    isNumber(number) {
+      return this.text === String(number);
+    },
+  });
+
+  const CustomScreenPageObject = ScreenPageObject.extend({
+    isNine() {
+      return this.isNumber(9);
+    }
+  });
+
+  const page = PageObject.create({
+    context: this,
+
+    getScreen() {
+      return CustomScreenPageObject.create({
         context: this.context,
         scope: '.screen',
       });
