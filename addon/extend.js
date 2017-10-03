144c4febef40df967bc6af99161912fca9ddc74d
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-10-03 22:17:30 +0300

    add native-context to acceptance tests

--- addon/extend.js
+++ addon/extend.js
@@ -5,10 +5,17 @@ import { register as registerExecutionContext } from './-private/execution_conte
 
 import IntegrationNativeContext from './-private/execution_context/integration-native';
 import AcceptanceNativeContext from './-private/execution_context/acceptance-native';
+import IntegrationClassicContext from './-private/execution_context/integration';
+import AcceptanceClassicContext from './-private/execution_context/acceptance';
 
-function useNativeDOMHelpers() {
-  registerExecutionContext('integration', IntegrationNativeContext);
-  registerExecutionContext('acceptance', AcceptanceNativeContext);
+function useNativeDOMHelpers(flag = true) {
+  if (flag) {
+    registerExecutionContext('integration', IntegrationNativeContext);
+    registerExecutionContext('acceptance', AcceptanceNativeContext);
+  } else {
+    registerExecutionContext('integration', IntegrationClassicContext);
+    registerExecutionContext('acceptance', AcceptanceClassicContext);
+  }
 }
 
 export { registerExecutionContext, useNativeDOMHelpers };
--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -2,22 +2,34 @@ import { module } from 'qunit';
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+import { useNativeDOMHelpers } from 'ember-cli-page-object/extend';
 
 const { RSVP: { Promise } } = Ember;
 
 export default function(name, options = {}) {
-  module(name, {
-    beforeEach() {
-      this.application = startApp();
-
-      if (options.beforeEach) {
-        return options.beforeEach.apply(this, arguments);
-      }
-    },
-
-    afterEach() {
-      let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
+  [false, true].forEach(_useNativeDOMHelpers => {
+    let moduleName = name;
+    if (_useNativeDOMHelpers) {
+      moduleName += ' [native-dom-helpers]';
     }
+
+    module(moduleName, {
+      beforeEach() {
+        this.application = startApp();
+
+        useNativeDOMHelpers(_useNativeDOMHelpers);
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach() {
+        useNativeDOMHelpers(false);
+
+        let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(() => destroyApp(this.application));
+      }
+    });
   });
 }
