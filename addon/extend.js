9e10e02106c75f28fcd72f7c6e3ab142efd9d4ef
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-11-11 22:56:12 +0200

    Add useNativeDOMHelpers helper

--- addon/extend.js
+++ addon/extend.js
@@ -1,4 +1,21 @@
 export { findElement } from './-private/extend/find-element';
 export { findElementWithAssert } from './-private/extend/find-element-with-assert';
 export { buildSelector, getContext, fullScope } from './-private/helpers';
-export { register as registerExecutionContext } from './-private/execution_context';
+import { register as registerExecutionContext } from './-private/execution_context';
+
+import IntegrationNativeDOMContext from './-private/execution_context/integration-native-dom';
+import AcceptanceNativeDOMContext from './-private/execution_context/acceptance-native-dom';
+import IntegrationEmberContext from './-private/execution_context/integration';
+import AcceptanceEmberContext from './-private/execution_context/acceptance';
+
+function useNativeDOMHelpers(flag = true) {
+  if (flag) {
+    registerExecutionContext('integration', IntegrationNativeDOMContext);
+    registerExecutionContext('acceptance', AcceptanceNativeDOMContext);
+  } else {
+    registerExecutionContext('integration', IntegrationEmberContext);
+    registerExecutionContext('acceptance', AcceptanceEmberContext);
+  }
+}
+
+export { registerExecutionContext, useNativeDOMHelpers };
