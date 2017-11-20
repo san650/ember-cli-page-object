1402a5e69adc37d7c92b7b25ff360a2a6cbd517f
Christopher Garrett <me@pzuraq.com>
2017-11-20 11:04:49 -0800

    feat(collection): New Collection API
    Adds a new collection API that allows users to pass in a PageObject
    directly, and treats the resulting collection more like an array. Users
    can opt-in to this new style by omitting `itemScope`, which is a field
    that would only be used by the legacy style of collections:
    
    ```js
    // old
    let page = create({
      items: collection({
        itemScope: 'li',
        item: {
          isActive: hasClass('active'),
          click: clickable()
        }
      })
    });
    
    page.items(0).click();
    page.items().filter(i => i.isActive)[0].click();
    
    // new
    let page = {
      items: collection({
        scope: 'li',
        isActive: hasClass('active'),
        click: clickable()
      })
    }
    
    page.items.objectAt(0).click();
    page.items.filter(i => i.isActive)[0].click();
    ```
    
    add documentation and deprecation warning

---
+++ addon/-private/properties/collection/main.js
@@ -0,0 +1,104 @@
+/* global Symbol */
+import Ember from 'ember';
+import { buildSelector, assign } from '../../helpers';
+import { create } from '../../create';
+import { count } from '../count';
+import Ceibo from 'ceibo';
+
+export class Collection {
+  constructor(definition, parent, key) {
+    this.definition = definition;
+    this.parent = parent;
+    this.key = key;
+
+    this._itemCounter = create({
+      count: count(definition.scope, {
+        resetScope: definition.resetScope,
+        testContainer: definition.testContainer
+      })
+    }, { parent });
+
+    this._items = [];
+  }
+
+  get length() {
+    return this._itemCounter.count;
+  }
+
+  objectAt(index) {
+    if (typeof this._items[index] === 'undefined') {
+      let { definition, parent, key } = this;
+      let scope = buildSelector({}, definition.scope, { at: index });
+
+      let finalizedDefinition = assign({}, definition);
+
+      finalizedDefinition.scope = scope;
+
+      let tree = create(finalizedDefinition, { parent });
+
+      // Change the key of the root node
+      Ceibo.meta(tree).key = `${key}[${index}]`;
+
+      this._items[index] = tree;
+    }
+
+    return this._items[index];
+  }
+
+  filter(...args) {
+    return this.toArray().filter(...args);
+  }
+
+  filterBy(...args) {
+    return this.toArray().filterBy(...args);
+  }
+
+  forEach(...args) {
+    return this.toArray().forEach(...args);
+  }
+
+  map(...args) {
+    return this.toArray().map(...args);
+  }
+
+  mapBy(...args) {
+    return this.toArray().mapBy(...args);
+  }
+
+  toArray() {
+    let { length } = this;
+
+    let array = Ember.A();
+
+    for (let i = 0; i < length; i++) {
+      array.push(this.objectAt(i));
+    }
+
+    return array;
+  }
+}
+
+if (typeof (Symbol) !== 'undefined' && Symbol.iterator) {
+  Collection.prototype[Symbol.iterator] = function() {
+    let i = 0;
+    let items = this.toArray();
+    let next = () => ({ done: i >= items.length, value: items[i++] });
+
+    return { next };
+  }
+}
+
+export function collection(definition) {
+  let descriptor = {
+    isDescriptor: true,
+
+    setup(node, key) {
+      // Set the value on the descriptor so that it will be picked up and applied by Ceibo.
+      // This does mutate the descriptor, but because `setup` is always called before the
+      // value is assigned we are guaranteed to get a new, unique Collection instance each time.
+      descriptor.value = new Collection(definition, node, key);
+    }
+  };
+
+  return descriptor;
+}
