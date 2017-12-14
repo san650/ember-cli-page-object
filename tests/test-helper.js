c1ee642839b16f1c1619eb069a983b0f717ab7fe
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-12-15 01:09:50 +0200

    Do not use error handling hack for ember >= 2.17

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import resolver from './helpers/resolver';
 import {
   setResolver
@@ -6,43 +5,5 @@ import {
 import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
-
-
-// ember@2.11.3 introduces a (breaking?) change in how the errors are propagated
-// in backburner which causes some test that were previously working, to fail.
-//
-// See https://github.com/emberjs/ember.js/pull/14898#issuecomment-285510703
-
-// We need to use this hack to test with >= 2.11
-function useHack() {
-  var [major, minor] = Ember.VERSION.split('.');
-  major = Number(major);
-  minor = Number(minor);
-
-  return (major === 2 && minor >= 11) || major > 2;
-}
-
-export function expectEmberError(assert, callback, matcher, message) {
-  if (useHack()) {
-    let origTestAdapter = Ember.Test.adapter;
-    let testError;
-    let TestAdapter = Ember.Test.QUnitAdapter.extend({
-      exception(error) {
-        testError = error;
-      }
-    });
-
-    Ember.run(() => { Ember.Test.adapter = TestAdapter.create(); });
-    callback();
-    Ember.run(() => {
-      Ember.Test.adapter.destroy();
-    });
-    Ember.Test.adapter = origTestAdapter;
-
-    assert.ok(testError && testError.message.match(matcher), message);
-  } else {
-    assert.throws(callback, matcher, message);
-  }
-}
 
 start();
