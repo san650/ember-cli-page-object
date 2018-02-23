9d565b85d64ad506445fdf8eb920c1a4c5a62695
Ben Demboski <ben@turbopatent.com>
2018-02-23 11:29:18 -0800

    Fix wait import
    See comment in file

--- addon/-private/compatibility.js
+++ addon/-private/compatibility.js
@@ -50,7 +50,12 @@ if (require.has('@ember/test-helpers')) {
 }
 
 if (require.has('ember-test-helpers/wait')) {
-  helpers.wait = require('ember-test-helpers/wait').default;
+  // This is implemented as a function that calls `ember-test-helpers/wait`
+  // rather than just assigning `helpers.wait = require(...).default` because
+  // since this code executes while modules are initially loading, under certain
+  // conditions `ember-test-helpers/wait` can still be in the pending state
+  // at this point, so its exports are still undefined.
+  helpers.wait = (...args) => require('ember-test-helpers/wait').default(...args);
 } else {
   helpers.wait = () => {
     throw new Error('ember-test-helpers or @ember/test-helpers must be installed');
