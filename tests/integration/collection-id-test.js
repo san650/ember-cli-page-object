4e5387c2f5f5eb7598257053c561aceb7fe9988a
Ruslan Hrabovyi <kudgo.test@gmail.com>
2018-07-26 00:38:58 +0300

    Guard for @ember/test-helpers

--- tests/integration/collection-id-test.js
+++ tests/integration/collection-id-test.js
@@ -1,37 +1,41 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { create, collection, attribute } from 'ember-cli-page-object';
+import require from 'require';
 
-const page = create({
-  items: collection('ul>li', {
-    id: attribute('data-test-uuid')
-  })
-});
+if (require.has('@ember/test-helpers')) {
+  const { render } = require('@ember/test-helpers')
 
-const { items } = page;
-
-module('Integration | collection id', function(hooks) {
-  setupRenderingTest(hooks);
-
-  test('getById works', async function(assert) {
-    await render(hbs`<ul>
-      <li data-test-uuid="uuid1">UUID1</li>
-      <li data-test-uuid="uuid2">UUID2</li>
-    </ul>`);
-
-    const first = items.getById('uuid1');
-    assert.equal(first.text, 'UUID1');
+  const page = create({
+    items: collection('ul>li', {
+      id: attribute('data-test-uuid')
+    })
   });
 
-  test('enumerating by id works', async function(assert) {
-    await render(hbs`<ul>
-      <li data-test-uuid="uuid1">UUID1</li>
-      <li data-test-uuid="uuid2">UUID2</li>
-    </ul>`);
+  const { items } = page;
 
-    const { uuid1 } = items;
-    assert.equal(uuid1.text, 'UUID1');
+  module('Integration | collection id', function(hooks) {
+    setupRenderingTest(hooks);
+
+    test('getById works', async function(assert) {
+      await render(hbs`<ul>
+        <li data-test-uuid="uuid1">UUID1</li>
+        <li data-test-uuid="uuid2">UUID2</li>
+      </ul>`);
+
+      const first = items.getById('uuid1');
+      assert.equal(first.text, 'UUID1');
+    });
+
+    test('enumerating by id works', async function(assert) {
+      await render(hbs`<ul>
+        <li data-test-uuid="uuid1">UUID1</li>
+        <li data-test-uuid="uuid2">UUID2</li>
+      </ul>`);
+
+      const { uuid1 } = items;
+      assert.equal(uuid1.text, 'UUID1');
+    });
   });
-});
+}
