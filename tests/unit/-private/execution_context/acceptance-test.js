a77b71b83ce9dbd37d41238c732907b1edbb4a0b
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2019-02-26 23:59:56 +0200

    Test execution contexts async behavior

---
+++ tests/unit/-private/execution_context/acceptance-test.js
@@ -0,0 +1,76 @@
+import { test } from 'ember-qunit';
+import moduleForAcceptance from 'dummy/tests/helpers/module-for-acceptance';
+import { create, visitable } from 'ember-cli-page-object'
+import { createClickTrackerComponent, ClickTrackerDef } from './helpers';
+
+const node = create(
+  Object.assign({}, ClickTrackerDef, {
+    visit: visitable('/')
+  })
+);
+
+moduleForAcceptance('Acceptance | acceptance context | actions', {
+  beforeEach(assert) {
+    this.application.register(
+      'component:test-component',
+      createClickTrackerComponent(assert)
+    );
+
+    return node.visit();
+  }
+});
+
+test('async invocations', async function(assert) {
+  await node.click()
+  await node.click();
+
+  assert.verifySteps([
+    'begin #0',
+    'complete #0',
+    'begin #1',
+    'complete #1'
+  ])
+});
+
+test('sync invocations', async function(assert) {
+  node.click()
+  node.click();
+
+  return window.andThen(() => {
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1'
+    ])
+  });
+});
+
+test('sync chained invocations', async function(assert) {
+  node.click()
+    .click();
+
+  return window.andThen(() => {
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1',
+    ])
+  })
+});
+
+test('async chained invocations', async function(assert) {
+  await node.click()
+    .click();
+
+  return window.andThen(() => {
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1'
+    ])
+  })
+});
+
---
+++ tests/unit/-private/execution_context/integration-test.js
@@ -0,0 +1,77 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
+import { create } from 'ember-cli-page-object'
+import hbs from 'htmlbars-inline-precompile';
+import { createClickTrackerComponent, ClickTrackerDef } from './helpers';
+
+const node = create(ClickTrackerDef);
+
+moduleForComponent('', 'Integration | integration context | actions', {
+  integration: true,
+
+  beforeEach(assert) {
+    this.register('component:action-tracker', createClickTrackerComponent(assert))
+
+    node.setContext(this);
+
+    this.render(hbs`{{action-tracker}}`);
+  },
+
+  afterEach() {
+    node.removeContext();
+  }
+});
+
+test('async invocations', async function(assert) {
+  await node.click()
+  await node.click();
+
+  assert.verifySteps([
+    'begin #0',
+    'complete #0',
+    'begin #1',
+    'complete #1'
+  ])
+});
+
+test('sync invocations', async function(assert) {
+  node.click()
+  node.click();
+
+  return wait().then(() => {
+    assert.verifySteps([
+      'begin #0',
+      'begin #1',
+      'complete #0',
+      'complete #1'
+    ])
+  });
+});
+
+test('sync chained invocations', async function(assert) {
+  node.click()
+    .click();
+
+  return wait().then(() => {
+    assert.verifySteps([
+      'begin #0',
+      'begin #1',
+      'complete #0',
+      'complete #1',
+    ])
+  })
+});
+
+test('async chained invocations', async function(assert) {
+  await node.click()
+    .click();
+
+  return wait().then(() => {
+    assert.verifySteps([
+      'begin #0',
+      'begin #1',
+      'complete #0',
+      'complete #1'
+    ])
+  })
+});
