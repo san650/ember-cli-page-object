91a5b1ef94d16731d13352ed3c586d5497edf4af
Ruslan Hrabovyi <kudgo.test@gmail.com>
2019-03-02 20:39:40 +0200

    Disable jquery in ember-try scenarios for modern Ember versions
    ember@3.9-beta started to raise a deprecation when Ember.$ is used

--- config/ember-try.js
+++ config/ember-try.js
@@ -80,6 +80,11 @@ module.exports = function() {
             devDependencies: {
               'ember-source': urls[0]
             }
+          },
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': false
+            })
           }
         },
         {
@@ -88,6 +93,11 @@ module.exports = function() {
             devDependencies: {
               'ember-source': urls[1]
             }
+          },
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': false
+            })
           }
         },
         {
@@ -96,6 +106,11 @@ module.exports = function() {
             devDependencies: {
               'ember-source': urls[2]
             }
+          },
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': false
+            })
           }
         },
         {
