ba26739809c8c67034585cef91cd374bbfa48abf
Ruslan Hrabovyi <kudgo.test@gmail.com>
2019-11-06 01:24:01 +0200

    Remove exectution context fallback error test

--- tests/unit/-private/execution_context/rfc268-test.js
+++ tests/unit/-private/execution_context/rfc268-test.js
@@ -1,6 +1,6 @@
 import require from 'require';
 import { test, module } from 'qunit';
-import { setupRenderingTest, setupTest } from 'ember-qunit';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { create } from 'ember-cli-page-object'
 import { createClickTrackerComponent, ClickTrackerDef } from './helpers';
@@ -10,97 +10,65 @@ const node = create(ClickTrackerDef);
 if (require.has('@ember/test-helpers')) {
   const { render, settled } = require('@ember/test-helpers');
 
-  module('Integration | rfc268 context', function() {
-    module('getExecutionContext', function(hooks) {
-      setupTest(hooks);
+  module('Integration | rfc268 context | actions', function(hooks) {
+    setupRenderingTest(hooks);
 
-      let origGetContext;
-      let compatModule;
+    hooks.beforeEach(function(assert) {
+      this.owner.register('component:action-tracker', createClickTrackerComponent(assert))
 
-      hooks.beforeEach(function() {
-        compatModule = require('ember-cli-page-object/test-support/-private/compatibility');
-        origGetContext = compatModule.getContext;
-      });
+      return render(hbs`{{action-tracker}}`);
+    })
 
-      hooks.afterEach(function() {
-        compatModule.getContext = origGetContext;
+    test('sync invocations', async function(assert) {
+      node.click()
+      await node.click();
 
-        // Make sure we don't leave this module in a mutated state
-        window.require.unsee('ember-cli-page-object/test-support/-private/execution_context');
-      });
+      await settled();
 
-      test(`fails if can't be used as a fallback context`, async function(assert) {
-        compatModule.getContext = () => null
-
-        const a = create();
-
-        assert.throws(
-          () => a.isVisible,
-          'Can not detect test type. Please make sure you use the latest version of "@ember/test-helpers"'
-        );
-      });
+      assert.verifySteps([
+        'begin #0',
+        'begin #1',
+        'complete #0',
+        'complete #1'
+      ])
     });
 
-    module('actions', function(hooks) {
-      setupRenderingTest(hooks);
+    test('async invocations', async function(assert) {
+      await node.click()
+      await node.click();
 
-      hooks.beforeEach(function(assert) {
-        this.owner.register('component:action-tracker', createClickTrackerComponent(assert))
+      assert.verifySteps([
+        'begin #0',
+        'complete #0',
+        'begin #1',
+        'complete #1'
+      ])
+    });
 
-        return render(hbs`{{action-tracker}}`);
-      })
+    test('async chained invocations', async function(assert) {
+      await node.click().click();
 
-      test('sync invocations', async function(assert) {
-        node.click()
-        await node.click();
+      assert.verifySteps([
+        'begin #0',
+        'complete #0',
+        'begin #1',
+        'complete #1'
+      ])
+    });
 
-        await settled();
+    test('sync chained invocations', async function(assert) {
+      node.click().click();
 
-        assert.verifySteps([
-          'begin #0',
-          'begin #1',
-          'complete #0',
-          'complete #1'
-        ])
-      });
+      await settled();
+      await settled();
+      await settled();
 
-      test('async invocations', async function(assert) {
-        await node.click()
-        await node.click();
-
-        assert.verifySteps([
-          'begin #0',
-          'complete #0',
-          'begin #1',
-          'complete #1'
-        ])
-      });
-
-      test('async chained invocations', async function(assert) {
-        await node.click().click();
-
-        assert.verifySteps([
-          'begin #0',
-          'complete #0',
-          'begin #1',
-          'complete #1'
-        ])
-      });
-
-      test('sync chained invocations', async function(assert) {
-        node.click().click();
-
-        await settled();
-        await settled();
-        await settled();
-
-        assert.verifySteps([
-          'begin #0',
-          'complete #0',
-          'begin #1',
-          'complete #1',
-        ])
-      });
+      assert.verifySteps([
+        'begin #0',
+        'complete #0',
+        'begin #1',
+        'complete #1',
+      ])
     });
   });
 }
