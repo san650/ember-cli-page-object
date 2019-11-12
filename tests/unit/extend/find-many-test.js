f141e9cb11fa2f61f2c67fc812cf100f9f895ab7
Yuri Ratanov <organium@gmail.com>
2019-11-12 16:17:45 +0700

    add more tests

--- tests/unit/extend/find-many-test.js
+++ tests/unit/extend/find-many-test.js
@@ -47,5 +47,71 @@ if (require.has('@ember/test-helpers')) {
 
       assert.deepEqual(findMany(page, '.ipsum', {}), []);
     });
+
+    test('testContainer param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="ipsum"></span>
+        <div class="new-test-root">
+          <span class="ipsum"></span>
+          <span class="ipsum"></span>
+        </div>
+      `);
+
+      assert.deepEqual(findMany(page, '.ipsum', { testContainer: '.new-test-root' }), findAll('.new-test-root .ipsum'));
+    });
+
+    test('resetScope param', async function(assert) {
+      let page = create({ scope: 'my-page' });
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <div class="my-page">
+          <span class="ipsum"></span>
+          <span class="ipsum"></span>
+        </div>
+      `);
+
+      assert.deepEqual(findMany(page, '.lorem', { resetScope: true }), findAll('.lorem'));
+    });
+
+    test('contains param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <span class="lorem">Word</span>
+        <span class="lorem">Word</span>
+      `);
+
+      assert.deepEqual(findMany(page, '.lorem', { contains: 'Word' }), findAll('.lorem').slice(1, 3));
+    });
+
+    test('scope param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <span class="ipsum">
+          <span class="lorem"></span>
+          <span class="lorem"></span>
+        </span>
+      `);
+
+      assert.deepEqual(findMany(page, '.lorem', { scope: '.ipsum' }), findAll('.ipsum .lorem'));
+    });
+
+    test('visible param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem" style="display:none"></span>
+        <span class="lorem"></span>
+        <span class="lorem"></span>
+      `);
+
+      assert.deepEqual(findMany(page, '.lorem', { visible: true }), findAll('.lorem').slice(1, 3));
+    });
   });
 }
--- tests/unit/extend/find-one-test.js
+++ tests/unit/extend/find-one-test.js
@@ -7,7 +7,7 @@ import hbs from 'htmlbars-inline-precompile';
 import require from 'require';
 
 if (require.has('@ember/test-helpers')) {
-  const { find } = require('@ember/test-helpers');
+  const { find, findAll } = require('@ember/test-helpers');
 
   module(`Extend | findOne`, function(hooks) {
     setupRenderingTest(hooks);
@@ -44,6 +44,97 @@ if (require.has('@ember/test-helpers')) {
 
       assert.throws(() => findOne(page, '.unknown', {}),
         /Error: Element not found./);
+    });
+
+    test('testContainer param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="ipsum"></span>
+        <div class="new-test-root">
+          <span class="ipsum"></span>
+        </div>
+      `);
+
+      assert.equal(findOne(page, '.ipsum', { testContainer: '.new-test-root' }), find('.new-test-root .ipsum'));
+    });
+
+    test('resetScope param', async function(assert) {
+      let page = create({ scope: 'my-page' });
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <div class="my-page">
+          <span class="ipsum"></span>
+        </div>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { resetScope: true }), find('.lorem'));
+    });
+
+    test('at param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <span class="lorem"></span>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { at: 1 }), findAll('.lorem')[1]);
+    });
+
+    test('contains param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <span class="lorem">Word</span>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { contains: 'Word' }), findAll('.lorem')[1]);
+    });
+
+    test('last param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"><span class="lorem">Word</span></span>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { last: true }), findAll('.lorem')[1]);
+    });
+
+    test('pageObjectKey param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+      `);
+
+      assert.throws(() => findOne(page, '.unknown', {pageObjectKey: 'CUSTOM KEY'}),
+        /PageObject: 'page.CUSTOM KEY'/);
+    });
+
+    test('scope param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem"></span>
+        <span class="ipsum"><span class="lorem"></span></span>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { scope: '.ipsum' }), find('.ipsum .lorem'));
+    });
+
+    test('visible param', async function(assert) {
+      let page = create({});
+
+      await this.render(hbs`
+        <span class="lorem" style="display:none"></span>
+        <span class="lorem"></span>
+      `);
+
+      assert.equal(findOne(page, '.lorem', { visible: true }), findAll('.lorem')[1]);
     });
   });
 }
