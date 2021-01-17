0f23aed8eff791214da650f004db1c32d8084332
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-01-17 15:23:53 +0200

    convert `<LoginForm` to "@glimmer/component"

--- tests/dummy/app/components/login-form.js
+++ tests/dummy/app/components/login-form.js
@@ -1,19 +1,22 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import action from 'ember-cli-page-object/test-support/-private/action';
 
-export default Component.extend({
-  isError: false,
+export default class LoginForm extends Component {
+  @tracked
+  isError = false
 
-  message: null,
+  @tracked
+  message = null
 
-  actions: {
-    logIn() {
-      if (this.get('userName') === 'user@example.com' && this.get('password') === 'secret') {
-        this.set('message', 'Valid user!');
-        this.set('isError', false);
-      } else {
-        this.set('message', 'Invalid user!');
-        this.set('isError', true);
-      }
+  @action
+  logIn() {
+    if (this.args.userName === 'user@example.com' && this.args.password === 'secret') {
+      this.message = 'Valid user!';
+      this.isError = false;
+    } else {
+      this.message = 'Invalid user!';
+      this.isError = true;
     }
   }
-});
+}
