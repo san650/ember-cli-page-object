9f2c697a222e85f343d3de70b1bdcee13d45017b
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-02-10 00:04:59 +0200

    node aware local `$`

--- addon-test-support/-private/finders.js
+++ addon-test-support/-private/finders.js
@@ -13,11 +13,21 @@ function getContainer(pageObjectNode, options) {
     || getAdapter().testContainer;
 }
 
-export function $(selector, container) {
-  const { window } = getAdapter();
+function isWindow(window) {
+  return window && window.document;
+}
 
-  if (window) {
-    return jQuery(window)(selector, container)
+export function $(selector, container) {
+  const { window: adapterWindow } = getAdapter();
+
+  if (isWindow(adapterWindow)) {
+    if ((typeof window === 'undefined' || !isWindow(window))) {
+      // node.js environment
+      // see https://github.com/jquery/jquery/issues/3426#issuecomment-264649345
+      return jQuery(adapterWindow)(selector, container)
+    } else {
+      throw new Error('adapter `window` within browser environment is not supported');
+    }
   } else {
     return jQuery(selector, container);
   }
