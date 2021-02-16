1641a0c7f7c10483ffd6ee9bf6d620adfc2746bc
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-02-17 00:08:02 +0200

    migrate `create-url-argument`

--- tests/integration/deprecations/create-url-argument-test.js
+++ tests/integration/deprecations/create-url-argument-test.js
@@ -1,19 +1,36 @@
-import { moduleForProperty } from '../../helpers/properties';
+import { module, test } from 'qunit';
 import { create } from 'ember-cli-page-object';
+import deprecate from 'ember-cli-page-object/test-support/-private/deprecate';
+import require from 'require';
 
-moduleForProperty('Deprecation | create url argument', function(test, adapter) {
-  if (adapter === 'application') {
+if (require.has('@ember/test-helpers')) {
+  module('Deprecation | create url argument', function(hooks) {
+    hooks.beforeEach(function() {
+      deprecate.__calls = [];
+    });
+
+    hooks.afterEach(function() {
+      delete deprecate.__calls;
+    });
+
     test('it shows deprecation', async function(assert) {
       create('');
 
-      assert.expectDeprecation('Passing an URL argument to `create()` is deprecated');
+      assert.deepEqual(deprecate.__calls, [
+        [
+          'create-url-argument',
+          'Passing an URL argument to `create()` is deprecated',
+          '1.17.0',
+          '2.0.0'
+        ]
+      ])
     });
 
     test('it does not show deprecation', async function(assert) {
       create();
       create({});
 
-      assert.expectNoDeprecation();
+      assert.deepEqual(deprecate.__calls, [])
     });
-  }
-});
+  });
+}
