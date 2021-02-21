641d7bd09a14a666eefe76787fceeb05ee9951f0
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-02-21 16:22:09 +0200

    get rid of `Ember.deprecate(`

--- addon-test-support/-private/deprecate.js
+++ addon-test-support/-private/deprecate.js
@@ -1,23 +1,11 @@
-import { deprecate } from '@ember/application/deprecations';
-
 const NAMESPACE = 'ember-cli-page-object';
 
-// @todo: remove `_` after getting rid of the ember deprecate
-// for some reason renaming of the import leads to runrime issues
-export default function _deprecate(name, message, since, until) {
-  if (Array.isArray(_deprecate.__calls)) {
-    _deprecate.__calls.push([name, message, since, until]);
+export default function deprecate(name, message, since, until) {
+  if (Array.isArray(deprecate.__calls)) {
+    deprecate.__calls.push([name, message, since, until]);
   }
 
   const [major, minor] = since.split('.');
 
-  deprecate(message, false, {
-    id: `${NAMESPACE}.${name}`,
-    for: NAMESPACE,
-    since: {
-      enabled: since
-    },
-    until,
-    url: `https://ember-cli-page-object.js.org/docs/v${major}.${minor}.x/deprecations#${name}`,
-  });
+  console.warn(`DEPRECATION: ${message} [deprecation id: ${NAMESPACE}.${name}] See https://ember-cli-page-object.js.org/docs/v${major}.${minor}.x/deprecations#${name} for more details.`)
 }
