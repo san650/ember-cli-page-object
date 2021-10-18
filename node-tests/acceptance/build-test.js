1ceec6472635b5ed30d5aca254068adbe9625975
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-10-19 00:34:57 +0300

    restore build tests
    turns out ember-cli-addon-tests installs the beta of ember by default.
    Since the current beta is the new major 4.0.0, it's not compatible
    with the current ec-page-object dev dependencies setup,
    so we limit to v3 of ember for node tests here

--- node-tests/acceptance/build-test.js
+++ node-tests/acceptance/build-test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var fs = require('fs');
 var AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
 
-describe.skip('Acceptance: development build', function() {
+describe('Acceptance: development build', function() {
   this.timeout(600000);
 
   var app;
@@ -19,7 +19,8 @@ describe.skip('Acceptance: development build', function() {
   it('generates a development build', function() {
     return app
       .create('dummy', {
-        fixturesPath: 'node-tests/acceptance/fixtures'
+        fixturesPath: 'node-tests/acceptance/fixtures',
+        emberVersion: '^3.0.0'
       })
       .then(function() {
         return app.runEmberCommand('build', '-dev')
@@ -30,7 +31,8 @@ describe.skip('Acceptance: development build', function() {
   it('generates a production build', function() {
     return app
       .create('dummy', {
-        fixturesPath: 'node-tests/acceptance/fixtures'
+        fixturesPath: 'node-tests/acceptance/fixtures',
+        emberVersion: '^3.0.0'
       })
       .then(function() {
         return app.runEmberCommand('build', '-prod')
