7f922beb4a78835812c8a2c291383775abf700c7
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-12-27 19:59:18 +0200

    restore visitable test

--- tests/unit/-private/properties/visitable-test.ts
+++ tests/unit/-private/properties/visitable-test.ts
@@ -1,7 +1,11 @@
-import { moduleForProperty } from '../../../helpers/properties';
+import { setupApplicationTest } from '../../../helpers';
 import { create, visitable } from 'ember-cli-page-object';
+import { module, test } from 'qunit';
+import { currentURL } from '@ember/test-helpers';
 
-moduleForProperty('visitable', { needsVisit: true }, function(test) {
+module('visitable', function(hooks) {
+  setupApplicationTest(hooks);
+
   test("calls Ember's visit helper", async function(assert) {
     assert.expect(1);
 
@@ -11,8 +15,9 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable(expectedRoute)
     });
 
-    await this.adapter.await(page.foo());
-    assert.equal(this.adapter.currentURL(), expectedRoute);
+    await page.foo();
+
+    assert.equal(currentURL(), expectedRoute);
   });
 
   test('fills in dynamic segments', async function(assert) {
@@ -22,8 +27,8 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable('/users/:user_id/comments/:comment_id')
     });
 
-    await this.adapter.await(page.foo({ user_id: 5, comment_id: 1 }));
-    assert.equal(this.adapter.currentURL(), '/users/5/comments/1')
+    await page.foo({ user_id: 5, comment_id: 1 });
+    assert.equal(currentURL(), '/users/5/comments/1')
   });
 
   test("raises an exception if params aren't given for all dynamic segments", async function(assert) {
@@ -31,8 +36,8 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable('/users/:user_id')
     });
 
-    this.adapter.throws(assert, () => page.foo(), /Missing parameter for \'user_id\'/);
-    this.adapter.throws(assert, () => page.foo(), /page\.foo\(\)/);
+    assert.throws(() => page.foo(), /Missing parameter for \'user_id\'/);
+    assert.throws(() => page.foo(), /page\.foo\(\)/);
   });
 
   test('appends query params to the path', async function(assert) {
@@ -42,8 +47,8 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable('/html-render')
     });
 
-    await this.adapter.await(page.foo({ hello: 'world', lorem: 'ipsum' }));
-    assert.equal(this.adapter.currentURL(), '/html-render?hello=world&lorem=ipsum')
+    await page.foo({ hello: 'world', lorem: 'ipsum' });
+    assert.equal(currentURL(), '/html-render?hello=world&lorem=ipsum')
   });
 
   test('accepts both dynamic segments and query params', async function(assert) {
@@ -53,8 +58,8 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable('/users/:user_id/comments/:comment_id')
     });
 
-    await this.adapter.await(page.foo({ user_id: 5, comment_id: 1, hello: 'world', lorem: 'ipsum' }));
-    assert.equal(this.adapter.currentURL(), '/users/5/comments/1?hello=world&lorem=ipsum');
+    await page.foo({ user_id: 5, comment_id: 1, hello: 'world', lorem: 'ipsum' });
+    assert.equal(currentURL(), '/users/5/comments/1?hello=world&lorem=ipsum');
   });
 
   test('fills in encoded dynamic segments', async function(assert) {
@@ -64,7 +69,7 @@ moduleForProperty('visitable', { needsVisit: true }, function(test) {
       foo: visitable('/users/:user_id/comments/:comment_id')
     });
 
-    await this.adapter.await(page.foo({ user_id: 'a/user', comment_id: 1 }));
-    assert.equal(this.adapter.currentURL(), '/users/a%2Fuser/comments/1');
+    await page.foo({ user_id: 'a/user', comment_id: 1 });
+    assert.equal(currentURL(), '/users/a%2Fuser/comments/1');
   });
 });
