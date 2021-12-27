d5b5e269c5247243619ef9225c2f0d7ed0dfa8cb
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-12-27 18:30:32 +0200

    restore "createTemplate(" and finder test methods

--- tests/helpers/index.ts
+++ tests/helpers/index.ts
@@ -5,11 +5,40 @@ import {
 
 import { setAdapter, RFC268Adapter } from 'ember-cli-page-object';
 
+import hbs from 'htmlbars-inline-precompile';
+import require from 'require';
+import { TestContext as DefaultTestContext } from 'ember-test-helpers';
+
+export interface TestContext extends DefaultTestContext {
+  [k: string]: unknown;
+
+  createTemplate(
+    template: string,
+    options?: {
+      useAlternateContainer?: boolean
+    }
+  ): Promise<unknown>
+
+  findExternal(selector: string): JQuery;
+}
+
+/* eslint-disable ember/new-module-imports */
+// @ts-expect-error
+let { $ } = globalThis.Ember || {};
+/* eslint-enable ember/new-module-imports */
+
+if (typeof $ !== 'function') {
+  $ = require('jquery').default;
+}
+
+function render(...args: unknown[]) {
+  return require('@ember/test-helpers').render(...args);
+}
+
 export function setupApplicationTest(hooks: NestedHooks) {
   upstreamSetupApplicationTest(hooks);
 
   hooks.beforeEach(function() {
-    debugger
     setAdapter(new RFC268Adapter());
   });
 
@@ -21,8 +50,27 @@ export function setupApplicationTest(hooks: NestedHooks) {
 export function setupRenderingTest(hooks: NestedHooks) {
   upstreamSetupRenderingTest(hooks);
 
-  hooks.beforeEach(function() {
+  hooks.beforeEach(function(this: TestContext) {
     setAdapter(new RFC268Adapter());
+
+    const testContext = this;
+
+    this.createTemplate = function(template, options): Promise<unknown> {
+      if (options && options.useAlternateContainer) {
+        // The idea is to render the HTML outside the testing container so we
+        // render an empty component
+        $('#alternate-ember-testing').html(template);
+        testContext.set('raw', '');
+      } else {
+        testContext.set('raw', template);
+      }
+
+      return render(hbs`{{html-render html=this.raw}}`);
+    }
+
+    this.findExternal = function(selector: string): JQuery {
+      return $(selector, '#alternate-ember-testing');
+    }
   });
 
   hooks.afterEach(function() {
