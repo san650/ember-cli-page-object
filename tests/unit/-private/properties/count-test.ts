306960188e24357e1a874d7ff55672eea5a930d1
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-12-27 18:37:27 +0200

    restore count test

--- tests/unit/-private/properties/count-test.ts
+++ tests/unit/-private/properties/count-test.ts
@@ -1,13 +1,16 @@
-import { moduleForProperty } from '../../../helpers/properties';
+import { setupRenderingTest, TestContext } from '../../../helpers';
 import { create, count } from 'ember-cli-page-object';
+import { module, test } from 'qunit';
 
-moduleForProperty('count', function(test) {
-  test('returns the number of elements that match the selector', async function(assert) {
+module('count', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('returns the number of elements that match the selector', async function(this: TestContext, assert) {
     let page = create({
       foo: count('span')
     });
 
-    await this.adapter.createTemplate(this, page, `
+    await this.createTemplate(`
       <span></span>
       <span></span>
     `);
@@ -15,22 +18,22 @@ moduleForProperty('count', function(test) {
     assert.equal(page.foo, 2);
   });
 
-  test('returns 0 when the no element is matched', async function(assert) {
+  test('returns 0 when the no element is matched', async function(this: TestContext, assert) {
     let page = create({
       foo: count('span')
     });
 
-    await this.adapter.createTemplate(this, page);
+    await this.createTemplate('');
 
     assert.equal(page.foo, 0);
   });
 
-  test('looks for elements inside the scope', async function(assert) {
+  test('looks for elements inside the scope', async function(this: TestContext, assert) {
     let page = create({
       foo: count('span', { scope: '.scope' })
     });
 
-    await this.adapter.createTemplate(this, page, `
+    await this.createTemplate(`
       <div><span></span></div>
       <div class="scope"><span></span><span></span></div>
     `);
@@ -38,14 +41,14 @@ moduleForProperty('count', function(test) {
     assert.equal(page.foo, 2);
   });
 
-  test("looks for elements inside page's scope", async function(assert) {
+  test("looks for elements inside page's scope", async function(this: TestContext, assert) {
     let page = create({
       scope: '.scope',
 
       foo: count('span')
     });
 
-    await this.adapter.createTemplate(this, page, `
+    await this.createTemplate(`
       <div><span></span></div>
       <div class="scope"><span></span><span></span></div>
     `);
@@ -53,14 +56,14 @@ moduleForProperty('count', function(test) {
     assert.equal(page.foo, 2);
   });
 
-  test('resets scope', async function(assert) {
+  test('resets scope', async function(this: TestContext, assert) {
     let page = create({
       scope: '.scope',
 
       foo: count('span', { resetScope: true })
     });
 
-    await this.adapter.createTemplate(this, page, `
+    await this.createTemplate(`
       <div class="scope"></div>
       <div><span></span></div>
     `);
@@ -68,23 +71,23 @@ moduleForProperty('count', function(test) {
     assert.equal(page.foo, 1);
   });
 
-  test('looks for elements outside the testing container', async function(assert) {
+  test('looks for elements outside the testing container', async function(this: TestContext, assert) {
     let page = create({
       foo: count('span', { testContainer: '#alternate-ember-testing' })
     });
 
-    await this.adapter.createTemplate(this, page, '<span></span><span></span>', { useAlternateContainer: true });
+    await this.createTemplate('<span></span><span></span>', { useAlternateContainer: true });
 
     assert.equal(page.foo, 2);
   });
 
-  test('looks for elements within test container specified at node level', async function(assert) {
+  test('looks for elements within test container specified at node level', async function(this: TestContext, assert) {
     let page = create({
       testContainer: '#alternate-ember-testing',
       foo: count('span')
     });
 
-    await this.adapter.createTemplate(this, page, '<span></span><span></span>', { useAlternateContainer: true });
+    await this.createTemplate('<span></span><span></span>', { useAlternateContainer: true });
 
     assert.equal(page.foo, 2);
   });
