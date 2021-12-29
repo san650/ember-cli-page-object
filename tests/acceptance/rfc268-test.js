3617ffbf582bc5d8bec1329a01bfa912cd35acb2
Ruslan Hrabovyi <kudgo.test@gmail.com>
2021-12-29 02:58:51 +0200

    fixup: remove redundant adapter setup

--- tests/acceptance/rfc268-test.js
+++ tests/acceptance/rfc268-test.js
@@ -1,23 +1,13 @@
 import { module, test } from 'qunit';
 import {
-  setAdapter,
   create,
   value,
   visitable,
-  RFC268Adapter,
 } from 'ember-cli-page-object';
 import { setupApplicationTest } from '../helpers';
 
 module('Acceptance | rfc268', function (hooks) {
   setupApplicationTest(hooks);
-
-  hooks.beforeEach(function () {
-    setAdapter(new RFC268Adapter());
-  });
-
-  hooks.afterEach(function () {
-    setAdapter(null);
-  });
 
   let page = create({
     visit: visitable('/calculator'),
