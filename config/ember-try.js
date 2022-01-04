00ef006d3d8c6ad80f71dbcfc89072d20ae35d98
Ruslan Hrabovyi <kudgo.test@gmail.com>
2022-01-05 00:51:26 +0200

    remove really old scenarios which rely on bower

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,40 +11,6 @@ module.exports = function () {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.4',
-          bower: {
-            dependencies: {
-              ember: 'components/ember#lts-2-4',
-            },
-            resolutions: {
-              ember: 'lts-2-4',
-            },
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null,
-              'ember-cli-qunit': '^4.0.0',
-              'ember-qunit': null,
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              ember: 'components/ember#lts-2-8',
-            },
-            resolutions: {
-              ember: 'lts-2-8',
-            },
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null,
-            },
-          },
-        },
-        {
           name: 'ember-lts-2.12',
           npm: {
             devDependencies: {
