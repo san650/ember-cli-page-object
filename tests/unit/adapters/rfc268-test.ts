b553b8a0dfe00206f8817719aad2bf128f3af9c8
Ruslan Hrabovyi <kudgo.test@gmail.com>
2022-01-27 22:16:17 +0200

    get rid of `require` usages
    It does not play well with embroider,
    and we are safe to assume that`@ember/test-helpers` exists
    
    Also needed to completely remove addon-exports test cause it's
    based on `require(` usage.

--- tests/unit/adapters/rfc268-test.ts
+++ tests/unit/adapters/rfc268-test.ts
@@ -1,5 +1,5 @@
-import require from 'require';
 import { test, module } from 'qunit';
+import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from '../../helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { create } from 'ember-cli-page-object'
@@ -8,66 +8,62 @@ import { TestContext } from 'ember-test-helpers';
 
 const node = create(ClickTrackerDef);
 
-if (require.has('@ember/test-helpers')) {
-  const { render } = require('@ember/test-helpers');
+module('Integration | rfc268 adapter | actions', function(hooks) {
+  setupRenderingTest(hooks);
 
-  module('Integration | rfc268 adapter | actions', function(hooks) {
-    setupRenderingTest(hooks);
+  hooks.beforeEach(function(this: TestContext, assert) {
+    this.owner.register('component:action-tracker', createClickTrackerComponent(assert))
 
-    hooks.beforeEach(function(this: TestContext, assert) {
-      this.owner.register('component:action-tracker', createClickTrackerComponent(assert))
+    return render(hbs`{{action-tracker}}`);
+  })
 
-      return render(hbs`{{action-tracker}}`);
-    })
+  test('sync invocations', async function(assert) {
+    node.click()
+    node.click();
 
-    test('sync invocations', async function(assert) {
-      node.click()
-      node.click();
+    await node;
 
-      await node;
-
-      assert.verifySteps([
-        'begin #0',
-        'begin #1',
-        'complete #0',
-        'complete #1'
-      ])
-    });
-
-    test('async invocations', async function(assert) {
-      await node.click()
-      await node.click();
-
-      assert.verifySteps([
-        'begin #0',
-        'complete #0',
-        'begin #1',
-        'complete #1'
-      ])
-    });
-
-    test('async chained invocations', async function(assert) {
-      await node.click().click();
-
-      assert.verifySteps([
-        'begin #0',
-        'complete #0',
-        'begin #1',
-        'complete #1'
-      ])
-    });
-
-    test('sync chained invocations', async function(assert) {
-      node.click().click();
-
-      await node;
-
-      assert.verifySteps([
-        'begin #0',
-        'complete #0',
-        'begin #1',
-        'complete #1',
-      ])
-    });
+    assert.verifySteps([
+      'begin #0',
+      'begin #1',
+      'complete #0',
+      'complete #1'
+    ])
   });
-}
+
+  test('async invocations', async function(assert) {
+    await node.click()
+    await node.click();
+
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1'
+    ])
+  });
+
+  test('async chained invocations', async function(assert) {
+    await node.click().click();
+
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1'
+    ])
+  });
+
+  test('sync chained invocations', async function(assert) {
+    node.click().click();
+
+    await node;
+
+    assert.verifySteps([
+      'begin #0',
+      'complete #0',
+      'begin #1',
+      'complete #1',
+    ])
+  });
+});
