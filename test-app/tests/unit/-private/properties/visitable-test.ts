bd924c17a03b8b094ef08c3d96a25002e3bf96f1
Markus Gruber <markus.gruber@husqvarnagroup.com>
2024-01-08 13:28:02 +0100

    #619 chore: get rid of

--- test-app/tests/unit/-private/properties/visitable-test.ts
+++ test-app/tests/unit/-private/properties/visitable-test.ts
@@ -58,8 +58,30 @@ module('visitable', function(hooks) {
       foo: visitable('/users/:user_id/comments/:comment_id')
     });
 
-    await page.foo({ user_id: 5, comment_id: 1, hello: 'world', lorem: 'ipsum' });
-    assert.equal(currentURL(), '/users/5/comments/1?hello=world&lorem=ipsum');
+    await page.foo({
+      user_id: 5,
+      comment_id: 1,
+      hello: 'world',
+      lorem: 'ipsum',
+      reply_ids: [1, 2],
+      search: {
+        author: 'ember',
+        topic: 'lts',
+        deep: {
+          my_array: [99, 77],
+          my_value: true
+        }
+      }
+    });
+
+    assert.equal(
+      decodeURIComponent(currentURL()),
+      '/users/5/comments/1' +
+      '?hello=world&lorem=ipsum' +
+      '&reply_ids[]=1&reply_ids[]=2' +
+      '&search[author]=ember&search[topic]=lts' +
+      '&search[deep][my_array][]=99&search[deep][my_array][]=77&search[deep][my_value]=true'
+    );
   });
 
   test('fills in encoded dynamic segments', async function(assert) {
