f5fababea1076f7d806e6d52f67b4b249bfcc3b8
Ruslan Hrabovyi <kudgo.test@gmail.com>
2024-09-30 22:58:40 +0200

    get rid of the `{{action` helper usages

--- test-app/app/components/calculating-device.js
+++ test-app/app/components/calculating-device.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 import { computed as c } from '@ember/object';
 
 export default Component.extend({
@@ -11,33 +12,31 @@ export default Component.extend({
     },
   }),
 
-  actions: {
-    keyPress(key) {
-      let result = this.result;
-      let stack = this.stack;
-      let op = this.op;
+  keyPress: action(function (key) {
+    let result = this.result;
+    let stack = this.stack;
+    let op = this.op;
 
-      switch (key) {
-        case '+':
-        case '-':
-        case '=':
-          stack.push(parseInt(op + result));
-          this.set('result', '');
-          break;
-        default:
-          this.set('result', result + key.toString());
-          break;
-      }
+    switch (key) {
+      case '+':
+      case '-':
+      case '=':
+        stack.push(parseInt(op + result));
+        this.set('result', '');
+        break;
+      default:
+        this.set('result', result + key.toString());
+        break;
+    }
 
-      switch (key) {
-        case '-':
-          this.set('op', '-');
-          break;
-        case '=':
-          result = stack.reduce((result, value) => result + value, 0);
-          this.set('result', result.toString());
-          break;
-      }
-    },
-  },
+    switch (key) {
+      case '-':
+        this.set('op', '-');
+        break;
+      case '=':
+        result = stack.reduce((result, value) => result + value, 0);
+        this.set('result', result.toString());
+        break;
+    }
+  }),
 });
--- test-app/app/controllers/calculator.js
+++ test-app/app/controllers/calculator.js
@@ -1,6 +1,7 @@
 import { later } from '@ember/runloop';
 import Controller from '@ember/controller';
 import { computed as c } from '@ember/object';
+import { action } from '@ember/object';
 
 export default Controller.extend({
   init() {
@@ -20,46 +21,44 @@ export default Controller.extend({
     },
   }),
 
-  actions: {
-    keyPress(key, asyncOp) {
-      let exec = () => {
-        let result = this.expression;
-        let stack = this.stack;
-        let op = this.op;
+  keyPress: action(function (key, asyncOp) {
+    let exec = () => {
+      let result = this.expression;
+      let stack = this.stack;
+      let op = this.op;
 
-        switch (key) {
-          case '+':
-          case '-':
-          case '=':
-            stack.push(parseInt(op + result));
-            this.set('result', result);
-            this.set('expression', '');
-            break;
-          default:
-            this.set('expression', result + key.toString());
-            break;
-        }
-
-        switch (key) {
-          case '-':
-            this.set('op', '-');
-            break;
-          case '=':
-            result = stack.reduce((result, value) => result + value, 0);
-            this.set('expression', result.toString());
-            break;
-        }
-      };
-
-      if (asyncOp) {
-        this.set('loading', true);
-        later(() => {
-          this.set('loading', false);
-          exec();
-        }, 50);
-      } else {
-        exec();
+      switch (key) {
+        case '+':
+        case '-':
+        case '=':
+          stack.push(parseInt(op + result));
+          this.set('result', result);
+          this.set('expression', '');
+          break;
+        default:
+          this.set('expression', result + key.toString());
+          break;
       }
-    },
-  },
+
+      switch (key) {
+        case '-':
+          this.set('op', '-');
+          break;
+        case '=':
+          result = stack.reduce((result, value) => result + value, 0);
+          this.set('expression', result.toString());
+          break;
+      }
+    };
+
+    if (asyncOp) {
+      this.set('loading', true);
+      later(() => {
+        this.set('loading', false);
+        exec();
+      }, 50);
+    } else {
+      exec();
+    }
+  }),
 });
